efda46970c26488b4e0583044ecf268014b7c704
admin <admin@doctorpension.com>
2016-09-08 07:15:16 +0000

    Added Google Tag Manager code

--- includes/header.php
+++ includes/header.php
@@ -45,6 +45,15 @@
 if(isset($body_class)){
 	echo 'class="' . $body_class . '"';
 } ?>>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WTZBL8"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WTZBL8');</script>
+<!-- End Google Tag Manager -->
 		<div class="main-outercon" id="cs_section_01">
 			<span ></span>
 			<!--Begin header section-->
