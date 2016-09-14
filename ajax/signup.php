1cb4be7214e8ac3c67ce2c6c599a529cb8bb78a3
admin <admin@doctorpension.com>
2016-09-14 08:07:44 +0000

    Add email to db instead of emailing it

--- ajax/signup.php
+++ ajax/signup.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: application/json');
 include('../includes/application.php');
+require('../includes/db_connect.php');
 require_once VENDOR_DIR . '/autoload.php';
 require_once VENDOR_DIR . '/phpmailer/phpmailer/PHPMailerAutoload.php';
 require_once CLASSES_DIR . '/gmailer.php';
@@ -12,12 +13,17 @@ switch($_POST['action']){
 }
 
 function sendEmailForWaitingList($email){
-	$mail = new Gmailer();
-	$mail->addAddress(DEFAULT_EMAIL_FROM_EMAIL,'');
-	$mail->setSubject('Add email to waiting list');
-	$mail->setHTML('Add this email to list: '. $email);
-	if (!$mail->send()) {
-			echo "Mailer Error: " . $mail->getError();
+	global $mysqli;
+	$res = $mysqli->query("INSERT INTO waiting_list (email) values ('" . 
+	   		$mysqli->real_escape_string ($email) . "')");
+	if (!$res && $mysqli->errno != 1062){
+		$mail = new Gmailer();
+		$mail->addAddress(DEFAULT_EMAIL_FROM_EMAIL,'');
+		$mail->setSubject('Add email to waiting list, sql failed');
+		$mail->setHTML('Add this email to list: '. $email . PHP_EOL . 'The sql error: '. $mysqli->error);
+		if (!$mail->send()) {
+				echo "Mailer Error: " . $mail->getError();
+		}
 	}
 	echo json_encode(array('status' => 'success'));
 }
