80a20d9f4d36bcc33608004821da50993a3d67c4
admin <admin@doctorpension.com>
2016-09-18 06:42:05 +0000

    Add line break in security

--- index.php
+++ index.php
@@ -177,7 +177,7 @@ include('includes/header.php') ?>
 			<section class="content-maincont three-blockouter newthree-blk">
 				<div class="container">
 					<div class="firsl-mainblk">
-<h2>תרגישו בטוח המידע מאובטח ומשמש רק לצורך הייעוץ</h2>
+<h2>תרגישו בטוח,<br/> המידע מאובטח ומשמש רק לצורך הייעוץ</h2>
 					</div>
 					<div class="icon-outer"><img src="images/shield-icon.png" alt="המידע מאובטח" /></div>
 					<div class="listingouter">
